996e8e1b653e8a1837747212c69bbb712e47103d
Jacob Tsekhansky <tsekhansk@hartford.edu>
2025-04-14 21:17:26 -0400

    board printing

--- OthelloServer.java
+++ OthelloServer.java
@@ -19,13 +19,37 @@ public class OthelloServer {
     public void execute() {
 
         char[][] board = new char[board_size][board_size];
-        int currentPlayer = 1;
-
+        int currentPlayer = 1; // 1 - black, 2 - white
+        initializeboard(board);
         try {
             DataInputStream reader = new DataInputStream(socket.getInputStream());
             DataOutputStream writer = new DataOutputStream(socket.getOutputStream());
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void initializeboard(char[][] board) {
+        for (int k = 0; k < board_size; k++) {
+            for (int l = 0; l < board_size; l++) {
+                board[k][l] = empty;
+            }
+        }
+        // initial positions
+        board[3][3] = white;
+        board[3][4] = black;
+        board[4][4] = white;
+        board[4][3] = black;
+    }
+
+    private String boardToSend(char[][] board) {
+        String b = "\n  0 1 2 3 4 5 6 7\n";
+        for (int r = 0; r < board_size; r++) {
+            b = b + Integer.toString(r) + " ";
+            for (int c = 0; c < board_size; c++) {
+                b = b + board[r][c] + " ";
+            }
+        }
+        return b;
     }
 }
